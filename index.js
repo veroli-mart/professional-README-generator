12b414b829f03eb547529c79d43312dfe31fd2cf
Veronica Martinez <vmartinez6615@gmail.com>
2021-01-09 19:26:33 -0500

    added potential README layout

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 // TODO: Create an array of questions for user input
 const questions = [];
 
@@ -11,3 +12,6 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+/* licenses:
+ do checkbox for these:  apache license 2.0: https://github.com/moby/moby/blob/master/LICENSE, MIT license:https://github.com/git/git-scm.com/blob/master/MIT-LICENSE.txt, Boost Software License 1.0: https://github.com/boostorg/boost/blob/master/LICENSE_1_0.txt, Eclipse Public License 2.0: https://github.com/apache/iotdb/blob/master/licenses/Eclipse%20Public%20License%20version%202.0%20(EPL-2.0), Mozilla Public License 2.0: https://github.com/mozilla/openbadges-bakery/blob/master/LICENSE-MPL-2.0 */
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,69 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return "";
+  }
+  return "## License [!license](https:docs.github.com/en/free-pro-team@latest/github/creating-cloning-and-archiving-repositories/licensing-a-repository)";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function renderLicenseSection(license) {
+  if (!license) {
+    return "";
+  }
+  return "## License";
 }
 
-module.exports = generateMarkdown;
+// TODO: Create a function to generate markdown for README
+module.exports = (generateMarkdown) => {
+  const { bagde, link, ...data } = generateMarkdown;
+  return `# ${data.title}
+  
+  ${renderLicenseSection(badge)}
+  ${renderLicenseLink(link)}
+  ![GitHub License](https://img.shields.io/badge/License-${
+    data.license
+  }-blue.svg)
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ## Installation
+  To install necessary dependencies, run the following command:
+  \`\`\`
+  ${data.installCmd}
+  \`\`\`
+  ## Usage
+  ${data.usage}
+  ## License
+  This project is licensed under the ${data.license} license.
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  To run tests, run the following command:
+  \`\`\`
+  ${data.testCmd}
+  \`\`\`
+  ## Questions
+  If you have any questions about the repo, open an issue or contact me directly at ${
+    data.email
+  }. You can find more of my work at [${data.username}](https://github.com/${
+    data.username
+  }/).
+  `;
+};
