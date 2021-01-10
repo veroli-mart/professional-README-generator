8a3a4e0a06f280f243099463b617d49c0a0df1c3
Veronica Martinez <vmartinez6615@gmail.com>
2021-01-09 20:20:20 -0500

    adds inquirer questions

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown.js");
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
-
-/* licenses:
- do checkbox for these:  apache license 2.0: https://github.com/moby/moby/blob/master/LICENSE, MIT license:https://github.com/git/git-scm.com/blob/master/MIT-LICENSE.txt, Boost Software License 1.0: https://github.com/boostorg/boost/blob/master/LICENSE_1_0.txt, Eclipse Public License 2.0: https://github.com/apache/iotdb/blob/master/licenses/Eclipse%20Public%20License%20version%202.0%20(EPL-2.0), Mozilla Public License 2.0: https://github.com/mozilla/openbadges-bakery/blob/master/LICENSE-MPL-2.0 */
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the name of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "What does your project do?",
+    },
+    {
+      type: "input",
+      name: "installCmd",
+      message: "What command do you use to install the project?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What is the command to run your project?",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What license would you like for your project?",
+      choices: ["MIT", "Apache", "Mozilla", "GNU GPLv3"],
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "How can people contribute to this project?",
+    },
+    {
+      type: "input",
+      name: "testCmd",
+      message: "What is the command needed to run the tests?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email?",
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "What is your Github username",
+    },
+  ])
+  .then(function (promptAnswers) {
+    console.log(generateMarkdown(promptAnswers));
+    console.log(promptAnswers);
+  });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,39 +1,6 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (!license) {
-    return "";
-  }
-}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (!license) {
-    return "";
-  }
-  return "## License [!license](https:docs.github.com/en/free-pro-team@latest/github/creating-cloning-and-archiving-repositories/licensing-a-repository)";
-}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (!license) {
-    return "";
-  }
-  return "## License";
-}
-
-// TODO: Create a function to generate markdown for README
-module.exports = (generateMarkdown) => {
-  const { bagde, link, ...data } = generateMarkdown;
+function generateMarkdown(data) {
   return `# ${data.title}
   
-  ${renderLicenseSection(badge)}
-  ${renderLicenseLink(link)}
-  ![GitHub License](https://img.shields.io/badge/License-${
-    data.license
-  }-blue.svg)
   ## Description
   ${data.description}
   ## Table of Contents
@@ -60,10 +27,9 @@ module.exports = (generateMarkdown) => {
   ${data.testCmd}
   \`\`\`
   ## Questions
-  If you have any questions about the repo, open an issue or contact me directly at ${
-    data.email
-  }. You can find more of my work at [${data.username}](https://github.com/${
-    data.username
-  }/).
+  If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.username}](https://github.com/${data.username}/).
   `;
-};
+}
+
+// TODO: Create a function to generate markdown for README
+module.exports = generateMarkdown;
